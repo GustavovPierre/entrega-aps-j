c09b79771dd71307976defd26117b87dcb365402
GustavovPierre <38561106+GustavovPierre@users.noreply.github.com>
2020-06-24 14:01:53 -0300

    quase acabando

---
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/model/model/NorGate.java
@@ -0,0 +1,47 @@
+package br.pro.hashi.ensino.desagil.aps.model.model;
+
+
+public class NorGate extends Gate {
+        NandGate nandTop;
+        NandGate nandBottom;
+        NandGate nandRight;
+        NandGate nandNot;
+
+            public NorGate() {
+                super("NOR", 2);
+
+                nandTop = new NandGate();
+
+                nandBottom = new NandGate();
+
+                nandRight = new NandGate();
+
+                nandRight.connect(0, nandTop);
+                nandRight.connect(1, nandBottom);
+
+    nandNot = new NandGate();
+    nandNot.connect(0, nandRight);
+    nandNot.connect(1, nandRight);
+            }
+
+@Override
+public boolean read() {
+        return nandNot.read();
+    }
+
+        @Override
+        public void connect(int inputIndex, Emitter emitter) {
+            switch (inputIndex) {
+                case 0:
+                    nandTop.connect(0, emitter);
+                    nandTop.connect(1, emitter);
+                    break;
+                case 1:
+                    nandBottom.connect(0, emitter);
+                    nandBottom.connect(1, emitter);
+                    break;
+                default:
+                    throw new IndexOutOfBoundsException(inputIndex);
+            }
+        }
+}
